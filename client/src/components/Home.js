c26bb277217ec50bcfb8e55edebe60a03d35c604
Allen <asciicode@gmail.com>
2022-06-01 21:55:56 +1200

    init

--- client/src/components/Home.js
+++ client/src/components/Home.js
@@ -1,12 +1,18 @@
 import React, { useState, useEffect } from "react";
 import AxiosService from "../services/axios.service";
+import AuthService from "../services/auth.service";
 
 const Home = () => {
   const [searchText, setSearchText] = useState("");
   const [flickr, setFlickr] = useState([]);
+  const [currentUser, setCurrentUser] = useState(undefined);
 
   useEffect(() => {
-    // search();
+    const user = AuthService.getCurrentUser();
+    
+    if (user) {
+      setCurrentUser(user);
+    }
   }, []);
   function search() {
     // alert("You clicked me!"+searchText);
@@ -21,19 +27,27 @@ const Home = () => {
   }
   return (
     <div>
-      <div>
-      <input type="text"  placeholder="Search Flickr by Tags" onChange={(e) => setSearchText(e.target.value)}/><button type="button" className="btn btn-light" onClick={search}>Search</button>
-      </div>
-      <section className="card-container">
+      { currentUser ? (
+        <div>
+        <div >
+          <input type="text"  placeholder="Search Flickr by Tags" onChange={(e) => setSearchText(e.target.value)}/><button type="button" className="btn btn-light" onClick={search}>Search</button>
+        </div>
+        <section className="card-container">
         {flickr.items?.map((val, key) => {
             return (<div className="card" key={key}>
             <img src={val.media.m}/>  
             <p>{val.title}</p>  
             </div>);
         })}
-      </section>
-    </div>
+        </section>
+      </div>
+      ) : (
+        <div className="navbar-nav ms-auto">
+        </div>
+      )}
+    </div>  
   );
-};
+
+}
 
 export default Home;
